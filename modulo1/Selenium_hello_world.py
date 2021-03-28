86e70552a9eea818b673816765d28c8381cf61e0
Rodrigo Mejia <rmejia@anexinet.com>
2021-03-28 13:45:06 -0600

    driver path

--- modulo1/Selenium_hello_world.py
+++ modulo1/Selenium_hello_world.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 
-driver_path = '/Users/rodrigomejia/Downloads'
+driver_path = '/Users/rodrigomejia/Desktop/PythonSel/PythonSelenium2021/drivers/chromedriver'
 driver = webdriver.Chrome(executable_path=driver_path)
 
 driver.get('https://google.com')
 search_box = driver.find_element_by_name('q')
 search_box.send_keys('QA Minds')
 driver.quit()
+
